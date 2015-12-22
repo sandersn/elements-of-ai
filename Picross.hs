24218c7f0b9cccff52a192ed9412f6929149aa64
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-12-22 09:31:56 -0800

    Start an example Picross solver

---
+++ Picross.hs
@@ -0,0 +1,37 @@
+data Pix = Black | White deriving (Show, Eq)
+type Pattern = [Specifier]
+type Row = [Pix]
+data Specifier = Star | Plus | Num Int deriving (Show, Eq)
+
+-- TODO: It's possible to optimize `expand` by passing in an existing row as constraint
+-- but consuming a row is a lot harder than consuming an integer
+expand :: Pattern -> Int -> [Row]
+expand [] 0 = [[]]
+expand [] _ = []
+expand (Num i:pattern) n | i <= n = map (expansion++) (expand pattern (n - i))
+  where expansion = [Black | _ <- [0..n - 1]]
+expand (Num i:pattern) n = []
+expand (Star:[]) n = [[White | _ <- [0..n - 1]]]
+expand (Star:pattern) n = [[]] -- TODO: Write expansion for Plus and Star
+expand (Plus:pattern) 0 = []
+expand (Plus:[]) n = [[White | _ <- [0..n - 1]]]
+expand (Plus:pattern) n = [[White]] -- TODO: Write expansion for Plus and Star
+
+test input length expected = do
+  let actual = expand input length
+  putStr "\n\tinput: "
+  print input
+  putStr "\tactual: "
+  print actual
+  putStr "\texpected: "
+  print expected
+  if actual == expected then putStrLn "success" else putStrLn " !!! FAIL !!!"
+
+main = do
+  test [] 0 [[]] 
+  test [] 12 []
+  test [Num 2] 2 [[Black, Black]]
+  test [Num 2, Num 2] 4 [[Black, Black, Black, Black]]
+  test [Num 2, Star] 2 [[Black, Black]]
+  test [Num 2, Star] 4 [[Black, Black, White, White]]
+  test [Star] 2 [[White, White]]
