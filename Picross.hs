5fd0180a350a4b045b1c7fb1928b1849965545b3
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-12-22 09:38:07 -0800

    Use monad terminology to make things clearer.

--- Picross.hs
+++ Picross.hs
@@ -1,3 +1,4 @@
+import Control.Monad (mzero)
 data Pix = Black | White deriving (Show, Eq)
 type Pattern = [Specifier]
 type Row = [Pix]
@@ -6,16 +7,16 @@ data Specifier = Star | Plus | Num Int deriving (Show, Eq)
 -- TODO: It's possible to optimize `expand` by passing in an existing row as constraint
 -- but consuming a row is a lot harder than consuming an integer
 expand :: Pattern -> Int -> [Row]
-expand [] 0 = [[]]
-expand [] _ = []
+expand [] 0 = return []
+expand [] _ = mzero
 expand (Num i:pattern) n | i <= n = map (expansion++) (expand pattern (n - i))
   where expansion = [Black | _ <- [0..n - 1]]
-expand (Num i:pattern) n = []
-expand (Star:[]) n = [[White | _ <- [0..n - 1]]]
-expand (Star:pattern) n = [[]] -- TODO: Write expansion for Plus and Star
-expand (Plus:pattern) 0 = []
-expand (Plus:[]) n = [[White | _ <- [0..n - 1]]]
-expand (Plus:pattern) n = [[White]] -- TODO: Write expansion for Plus and Star
+expand (Num i:pattern) n = mzero 
+expand (Star:[]) n = return [White | _ <- [0..n - 1]]
+expand (Star:pattern) n = return [] -- TODO: Write expansion for Plus and Star
+expand (Plus:pattern) 0 = mzero 
+expand (Plus:[]) n = return [White | _ <- [0..n - 1]]
+expand (Plus:pattern) n = return [White] -- TODO: Write expansion for Plus and Star
 
 test input length expected = do
   let actual = expand input length
