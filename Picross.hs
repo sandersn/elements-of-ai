c13fdfb46908297e87afb7e529c7eb6b57085f14
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-12-25 15:30:13 -1000

    Test generate
    Also
    
    1. Use replicate a bit more.
    
    1. Rename Black/White/Unknown to be shorter and more readable

--- Picross.hs
+++ Picross.hs
@@ -1,9 +1,9 @@
 import Control.Monad (mzero)
-data Pix = Black | White | Unknown deriving (Show, Eq)
-pixEq Unknown _ = True -- (should never happen here)
-pixEq _ Unknown = True
-pixEq Black Black = True
-pixEq White White = True
+data Pix = B | W | X deriving (Show, Eq)
+pixEq X _ = True -- (should never happen here)
+pixEq _ X = True
+pixEq B B = True
+pixEq W W = True
 pixEq _ _ = False
 
 data Axis = Horizontal | Vertical
@@ -29,15 +29,15 @@ writeRow = undefined
 {-
 step through each candidate rowset:
   read matching row
-  skip if it has no unknown cells
+  skip if it has no X cells
   remove candidates that do not match row
   infer new row based on candidates
   write new row [if it has changed from the matching row]
-  stop when all rows [all cells] in the puzzle have no unknown cells
+  stop when all rows [all cells] in the puzzle have no X cells
   (or when a complete cycle fails to make a changeto the puzzle)
 -}
 finished :: Puzzle -> Bool
-finished = all $ all (/=Unknown)
+finished = all $ all (/=X)
 finished2 :: Candidateset -> Bool
 finished2 = all $ (==1) . length
 allRows :: Puzzle -> [Row]
@@ -46,8 +46,8 @@ allRows puzzle = rows ++ columns
         columns = [map (!!i) puzzle | i <- [0..length (head puzzle) - 1]]
 solve :: [Pattern] -> Int -> Int -> Puzzle
 solve patterns height width = loop (candidates, newPuzzle)
-  where newPuzzle = [replicate height Unknown | i <- [0..width -1]]
-        candidates = map (generate height width) (zip [0..] patterns)
+  where newPuzzle = replicate width (replicate height X)
+        candidates = generate height width patterns
 loop :: (Candidateset, Puzzle) -> Puzzle
 loop (candidates, puzzle) =
   if finished puzzle
@@ -66,18 +66,18 @@ loopBody puzzle (rowset, i) = undefined
 --        newRow = infer newSet
 -- TODO: It's possible to optimize `expand` by passing in an existing row as constraint
 -- but consuming a row is a lot harder than consuming an integer
-generate :: Int -> Int -> (Int, Pattern) -> [Row]
-generate height width (i, pattern) =
-  expand pattern (if i < height then height else width)
+generate :: Int -> Int -> [Pattern] -> Candidateset
+generate height width patterns = map generate' (zip [0..] patterns)
+  where generate' (i,pattern) = expand pattern (if i < width then width else height)
 expand :: Pattern -> Int -> [Row]
 expand [] 0 = return []
 expand [] _ = mzero
-expand (Num i:pattern) n | i <= n = expand' pattern Black n i
+expand (Num i:pattern) n | i <= n = expand' pattern B n i
 expand (Num i:pattern) _ = mzero 
-expand (Star:[]) n = return $ replicate n White
-expand (Star:pattern) n = concat [expand' pattern White n i | i <- [0..n - 1]]
+expand (Star:[]) n = return $ replicate n W
+expand (Star:pattern) n = concat [expand' pattern W n i | i <- [0..n - 1]]
 expand (Plus:pattern) 0 = mzero
-expand (Plus:pattern) n = concat [expand' pattern White n i | i <- [1..n]]
+expand (Plus:pattern) n = concat [expand' pattern W n i | i <- [1..n]]
 expand' :: Pattern -> Pix -> Int -> Int -> [Row]
 expand' pattern pix n i = 
   map (replicate i pix ++) (expand pattern (n - i))
@@ -88,52 +88,75 @@ infer :: Rowset -> Row
 infer [] = []
 infer rowset | head rowset == [] = []
 infer rowset = infer' (map head rowset) : infer (map tail rowset)
-  where infer' row | all (==Black) row = Black
-        infer' row | all (==White) row = White
-        infer' _ = Unknown
+  where infer' row | all (==B) row = B
+        infer' row | all (==W) row = W
+        infer' _ = X
+slingshot = [[Star, Num 2, Plus, Num 2, Star], 
+             [Star, Num 1, Plus, Num 1, Star], 
+             [Star, Num 3, Star], 
+             [Star, Num 1, Star], 
+             [Star, Num 1, Star], 
+             -- columns
+             [Star, Num 2, Star], 
+             [Star, Num 1, Plus, Num 1, Star], 
+             [Star, Num 3, Star], 
+             [Star, Num 1, Plus, Num 1, Star], 
+             [Star, Num 2, Star]]
 main = do
   putStrLn "\t*** Testing `expand` ***"
   testExpand [] 0 [[]]
   testExpand [] 12 []
-  testExpand [Num 2] 2 [[Black, Black]]
-  testExpand [Num 2, Num 2] 4 [[Black, Black, Black, Black]]
-  testExpand [Num 2, Star] 2 [[Black, Black]]
-  testExpand [Num 2, Star] 4 [[Black, Black, White, White]]
-  testExpand [Star] 2 [[White, White]]
-  testExpand [Star, Num 2] 4 [[White, White, Black, Black]]
-  testExpand [Star, Num 2, Star] 4 [[Black, Black, White, White],
-                                    [White, Black, Black, White],
-                                    [White, White, Black, Black]]
+  testExpand [Num 2] 2 [[B, B]]
+  testExpand [Num 2, Num 2] 4 [[B, B, B, B]]
+  testExpand [Num 2, Star] 2 [[B, B]]
+  testExpand [Num 2, Star] 4 [[B, B, W, W]]
+  testExpand [Star] 2 [[W, W]]
+  testExpand [Star, Num 2] 4 [[W, W, B, B]]
+  testExpand [Star, Num 2, Star] 4 [[B, B, W, W],
+                                    [W, B, B, W],
+                                    [W, W, B, B]]
   testExpand [Plus] 0 []
-  testExpand [Plus] 1 [[White]]
-  testExpand [Plus] 2 [[White, White]]
-  testExpand [Num 1, Plus, Num 1] 4 [[Black, White, White, Black]]
-  testExpand [Plus, Num 1, Plus] 4 [[White, Black, White, White],
-                                    [White, White, Black, White]]
+  testExpand [Plus] 1 [[W]]
+  testExpand [Plus] 2 [[W, W]]
+  testExpand [Num 1, Plus, Num 1] 4 [[B, W, W, B]]
+  testExpand [Plus, Num 1, Plus] 4 [[W, B, W, W],
+                                    [W, W, B, W]]
   testExpand [Star, Num 1, Plus, Num 1, Star]
              4
-             [[Black, White, Black, White],
-              [Black, White, White, Black],
-              [White, Black, White, Black]]
+             [[B, W, B, W],
+              [B, W, W, B],
+              [W, B, W, B]]
   putStrLn "\t*** Testing `constrain` ***"
-  testConstrain [Unknown] [] []
+  testConstrain [X] [] []
   testConstrain [] [[]] [[]]
-  testConstrain [Unknown] [[Black], [White]] [[Black], [White]]
-  testConstrain [Black] [[Black], [White]] [[Black]]
-  testConstrain [White] [[Black], [White]] [[White]]
-  testConstrain [Black, White, Unknown] [[Black, Black, Black],
-                                         [Black, White, White],
-                                         [Black, White, Black],
-                                         [White, White, White]]
-                                        [[Black, White, White],
-                                         [Black, White, Black]]
+  testConstrain [X] [[B], [W]] [[B], [W]]
+  testConstrain [B] [[B], [W]] [[B]]
+  testConstrain [W] [[B], [W]] [[W]]
+  testConstrain [B, W, X] [[B, B, B],
+                           [B, W, W],
+                           [B, W, B],
+                           [W, W, W]]
+                          [[B, W, W],
+                           [B, W, B]]
   putStrLn "\t*** Testing `infer` ***"
   testInfer [] []
   testInfer [[]] []
-  testInfer [[Black]] [Black]
-  testInfer [[White]] [White]
-  testInfer [[Black], [White]] [Unknown]
-  testInfer [[Black, White, Black], [Black, White, White]] [Black, White, Unknown]
+  testInfer [[B]] [B]
+  testInfer [[W]] [W]
+  testInfer [[B], [W]] [X]
+  testInfer [[B, W, B], [B, W, W]] [B, W, X]
+  putStrLn "\t***Testing `generate` ***"
+  testGenerate 5 5 slingshot
+                   [[[B,B,W,B,B]],
+                    [[B,W,B,W,W],[B,W,W,B,W],[B,W,W,W,B],[W,B,W,B,W],[W,B,W,W,B],[W,W,B,W,B]],
+                    [[B,B,B,W,W],[W,B,B,B,W],[W,W,B,B,B]],
+                    [[B,W,W,W,W],[W,B,W,W,W],[W,W,B,W,W],[W,W,W,B,W],[W,W,W,W,B]],
+                    [[B,W,W,W,W],[W,B,W,W,W],[W,W,B,W,W],[W,W,W,B,W],[W,W,W,W,B]],
+                    [[B,B,W,W,W],[W,B,B,W,W],[W,W,B,B,W],[W,W,W,B,B]],
+                    [[B,W,B,W,W],[B,W,W,B,W],[B,W,W,W,B],[W,B,W,B,W],[W,B,W,W,B],[W,W,B,W,B]],
+                    [[B,B,B,W,W],[W,B,B,B,W],[W,W,B,B,B]],
+                    [[B,W,B,W,W],[B,W,W,B,W],[B,W,W,W,B],[W,B,W,B,W],[W,B,W,W,B],[W,W,B,W,B]],
+                    [[B,B,W,W,W],[W,B,B,W,W],[W,W,B,B,W],[W,W,W,B,B]]]
 testInfer rowset expected = do
   let actual = infer rowset
   if actual == expected
@@ -170,4 +193,15 @@ testExpand input length expected = do
     print actual
     putStr "\texpected: "
     print expected
-
+testGenerate height width patterns expected = do
+  let actual = generate height width patterns
+  if actual == expected
+  then putStrLn ("success for " ++ show patterns)
+  else do
+    putStrLn " !!! FAIL !!!"
+    putStr "\tpatterns: "
+    print patterns
+    putStr "\tactual: "
+    print actual
+    putStr "\texpected: "
+    print expected
