a8575148fadd15306cb1ff8b28c5990c183bb3bb
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-01-10 14:19:37 -0800

    Add match4

--- src/chapter3/match.cljs
+++ src/chapter3/match.cljs
@@ -16,3 +16,19 @@
     (= (first p) (first s)) (match3 (rest p) (rest s))
     (= (first p) '?) (match3 (rest p) (rest s))
     :else false))
+(defn match4 [p s]
+  (def variables (atom {}))
+  (defn match-helper [p s]
+    (cond
+      (or (atom? p) (atom? s)) false
+      (empty? p) (empty? s)
+      (= (first p) (first s)) (match4 (rest p) (rest s))
+      (and
+       (= (count (first p)) 2)
+       (= (first (first p)) '?)
+       (match4 (rest p) (rest s)))
+      (do
+        (swap! variables (fn [vs] (assoc vs (first (rest (first p))) (first s))))
+        true)
+    :else false))
+  (if (match-helper p s) @variables false))
