99eab53a298986af1e2bfded726250f39803a9ad
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-01-13 07:19:27 -0800

    Clone match5 to final match code

--- src/chapter3/match.cljs
+++ src/chapter3/match.cljs
@@ -59,3 +59,29 @@
         true)
     :else false))
   (if (match-helper p s) @variables false))
+(defn match
+  "Note: ClojureScript doesn't have resolve so arbitrary predicates won't work there."
+  [p s]
+  (def variables (atom {}))
+  (defn match-helper [p s]
+    (cond
+      (or (atom? p) (atom? s)) false
+      (empty? p) (empty? s)
+      (= (first p) (first s)) (match5 (rest p) (rest s))
+      (and
+       (= (count (first p)) 2)
+       (= (first (first p)) '?)
+       (match5 (rest p) (rest s)))
+      (do
+        (swap! variables (fn [vs] (assoc vs (first (rest (first p))) (first s))))
+        true)
+      (and
+       (= (count (first p)) 2)
+       (not (= (first (first p)) '?))
+       ((resolve (first (first p))) (first s))
+       (match5 (rest p) (rest s)))
+      (do
+        (swap! variables (fn [vs] (assoc vs (first (rest (first p))) (first s))))
+        true)
+    :else false))
+  (if (match-helper p s) @variables false))
