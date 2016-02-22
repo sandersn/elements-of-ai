13f5ba9a3619d75a18c69073cf01fe159f390af5
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-02-21 17:26:24 -0800

    Fix up leibniz and refactor goal handling
    It compiles and runs! Doesn't get the right answer yet though. Also most
    of the rules are still missing.
    
    It doesn't use an explicit goal anymore. Instead when the first rule set
    fails to match, the control system switches to the next rule set and so
    on. A more sophisticated system might be nice as an extension, but this is
    pretty simple.

--- src/chapter3/leibniz.cljs
+++ src/chapter3/leibniz.cljs
@@ -7,56 +7,58 @@
 (defn nested-diff-x2? [d v]
   (= v (d :e1)))
 (def diff-sum-rule
-  ['differentiate
-   '(d (nested-diff-sum? e3) (? v1))
+  ['(d (nested-diff-sum? e3) (? v1))
    #(list '+ (list 'd (% :e1) (% :v1)) (list 'd (% :e2) (% :v1)))
    "diff-sum-rule"])
 (def diff-x-rule
-  ['differentiate
-   '(d (nested-diff-x1? e1) (nested-diff-x2? e2))
+  ['(d (nested-diff-x1? e1) (nested-diff-x2? e2))
    1
    "diff-x-rule"])
 ; other rules go here ...
 
 ; TODO: Clojure HAS to have a class/record facility to do this...
-(def rule-goal first)
-(def rule-pattern second)
-(defn rule-action [rule] (nth rule 2))
-(defn rule-name [rule] (nth rule 3))
+(def rule-pattern first)
+(defn rule-action [rule] second)
+(defn rule-name [rule] (nth rule 2))
+
 (def rules
-  [diff-sum-rule diff-x-rule])
+  [[diff-sum-rule diff-x-rule]
+   [; TODO: arithmetic simplication rules go here
+    ]])
+   
 ;;; control scheme ;;;
 ; (I don't know--that's just what the book calls it)
-(declare try-rule1)
+(declare try-rule)
 (defn try-rule-on-list [rule expressions]
   (if (empty? expressions)
     nil
-    (if-let [temp (try-rule1 rule (first expressions))]
+    (if-let [temp (try-rule rule (first expressions))]
       (cons temp (rest expressions))
       (when-let [temp (try-rule-on-list rule (rest expressions))]
         (cons (first expressions) temp)))))
-(defn fire [rule]
+(defn fire [rule d]
   (println (rule-name rule) " fires.")
-  ((rule-action rule)))
-(defn try-rule1 [rule expression]
-  (cond
-    (atom? expression) nil
-    (match-state (rule-pattern rule) expression) (fire rule)
-    :else (try-rule-on-list rule expression)))
+  ((rule-action rule) d))
 (defn try-rule [rule expression]
-  (if (= current-goal (rule-goal rule))
-    (try-rule1 rule expression)))
+  (if (atom? expression)
+    nil
+    (if-let [d (match-state (rule-pattern rule) expression)]
+      (fire rule d)
+      (try-rule-on-list rule expression))))
 (defn try-rules [rules formula]
-  (loop [rules-left rules]
+  (loop [rules-left rules
+         formula formula]
     (cond
       (empty? rules-left) nil ; no rules applied -- fail 
       (atom? formula) nil ; ??? (reduced to an atom?) -- fail
       :else (if-let [new-formula (try-rule (first rules-left) formula)]
               new-formula
               (recur (rest rules-left) formula)))))
-(defn control []
-  ; TODO: current-formula is unbound
-  (loop [current-formula '()]
-      (if-let [new-formula (try-rules rules current-formula)]
-        (recur new-formula)
-        current-formula)))
+(defn control [formula rules]
+  (loop [formula formula
+         rules rules]
+    (if-let [new-formula (try-rules (first rules) formula)]
+      (recur new-formula rules)
+      (if (empty? (rest rules))
+        formula
+        (recur formula (rest rules))))))
