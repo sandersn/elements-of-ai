6743a086c8d23ecc6f00b98c0c8a1a2c4a8d4978
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-05-01 16:01:16 -0700

    First half of painted-squares works
    1. Added tests
    2. Fixed bugs
    3. Cleaned up style -- marking constants is not done in Clojure because
    everything is constant by default.

--- src/chapter5/painted-squares.cljs
+++ src/chapter5/painted-squares.cljs
@@ -1,13 +1,23 @@
 (ns chapter5.painted-squares
   (:refer chapter3.match :only [match match-state atom?])
   (:refer clojure.test :only [with-test is run-tests]))
-(def pattern 
+                                        ; type Piece = [Symbol]
+                                        ; type Orientation = Int
+                                        ; type Placement = (Symbol, Orientation)
+                                        ; type State = [Placement]
+                                        ; type Patterns = Map Symbol Piece
+(def pattern ; pattern :: Patterns
   '{p1 [st ha gr st]
     p2 [bx st ha bx]
     p3 [st ha bx gr]
     p4 [gr gr ha bx]})
-(def ^:dynamic *box-width* 2)
-(def ^:dynamic *box-length* 2) ; not actually used
+(def box-width 2)
+(def box-length 2) ; not actually used
+(def south 0)
+(def east 1)
+(def north 2)
+(def west 3)
+
 (with-test
   (defn rotate-list [l n]
     (let [offset (- (count l) n)]
@@ -18,26 +28,56 @@
   (is (= [1 2 3] (rotate-list [1 2 3] 3)))
   (is (= [1 2 3] (rotate-list [1 2 3] 4))) ; should probably wrap around instead
 )
-(defn orient [piece orientation]
-  (rotate-list (pattern piece) (dec orientation)))
-(defn sides-ok [new-piece orientation current-state]
-  (if (empty? current-state)
-    true
-    (let [trial (orient new-piece orientation)
+(with-test
+  (defn orient [[piece orientation]]
+    "This is pretty much only needed because of map lookup for the piece's symbol.
+Note that orientation is 1-based on the book, but 0-based here."
+    (rotate-list (pattern piece) orientation))
+  (is (= '[st ha gr st] (orient '(p1 0))))
+  (is (= '[st st ha gr] (orient '(p1 1)))))
+(with-test
+  (defn match-north [trial state]
+    "(nth state (dec box-width)) looks like exactly one row up"
+    (= (trial north)
+       ((orient (nth state (dec box-width))) south)))
+  (is (match-north '[-- -- st --] '((-- 2) (p1 0))))
+  (is (match-north '[-- -- gr --] '((-- 2) (p1 2))))
+  (is (not (match-north '[-- -- st --] '((-- 2) (p1 2))))))
+(with-test
+  (defn match-west [trial state]
+    (= (trial west)
+       ((orient (first state)) east)))
+  (is (match-west '[st st st st] '((p1 1))))
+  (is (match-west '[ha ha ha ha] '((p1 0))))
+  (is (not (match-west '[st st st st] '((p1 0) (-- 2) (-- 3)))))
+  (is (not (match-west '[ha ha ha ha] '((p1 1)))))
+  (is (match-west '[-- -- -- st] '((p1 1))))
+  (is (match-west '[-- -- -- ha] '((p1 0))))
+)
+(with-test
+  (defn sides-ok [placement current-state]
+    (let [trial (orient placement)
           len (count current-state)]
       (cond
-        (= 0 (rem len *box-width*)) (return (match-north trial current-state))
-        (< len *box-width*) (return (match-west trial current-state))
-        :else (return (and (match-north trial current-state)
-                           (match-west trial current-state)))))))
-(defn match-north [trial state]
-  (= (trial 2) ;north side
-     ((orient (state (dec *box-width*))) 0)))
-(defn match-west [trial state]
-  (= (trial 3) ; west side of rotated new piece
-     ((orient (first state)) 2)))
-        
-
+                                        ;  So he's checking whether we're currently in the (1) leftmost column (2) first row (3) other.
+                                        ; there are really only four cases in this setup, but this method will extend to any size at all.
+        ; first entry -- no check needed
+        (= 0 len) true
+        ; first column -- check to the north (but not west)
+        (= 0 (rem len box-width)) (match-north trial current-state)
+        ; first row -- check to the west (but not north)
+        (< len box-width) (match-west trial current-state)
+        ; others -- check both north and west
+        :else (and (match-north trial current-state)
+                   (match-west trial current-state)))))
+  (is (sides-ok '(p1 0) '()))
+  (is (sides-ok '(p2 1) '()))
+  (is (sides-ok '(p1 0) '((-- 0) (p3 1))))
+  (is (sides-ok '(p1 0) '((-- 0) (p4 0))))
+  (is (sides-ok '(p1 0) '((p3 1))))
+  (is (sides-ok '(p1 0) '((p2 0))))
+  (is (sides-ok '(p1 0) '((p2 0) (p4 0) (-- 0))))
+)
 (defn main []
   (loop [pieces '#{p1 p2 p3 p4}]
     'p1))
