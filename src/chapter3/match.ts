c576ddc544dcf7761c53ba5ef59b1544e80a7fe8
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-05-17 21:57:42 -0700

    Basic implementation and tests for match2

--- src/chapter3/match.ts
+++ src/chapter3/match.ts
@@ -1,5 +1,16 @@
 import Cons = require("jscons");
 export function isAtom(x: any) {
-    return !Cons.instanceOf(x);
+    return x === null || !Cons.instanceOf(x);
 }
-
+export function match2(p: any, s: any): boolean {
+    if (isAtom(p)) {
+        return isAtom(s);
+    }
+    else if (isAtom(s)) {
+        return false;
+    }
+    else if (match2(p.head(), s.head())) {
+        return match2(p.tail(), s.tail());
+    }
+    return false;
+}
--- src/chapter3/test.ts
+++ src/chapter3/test.ts
@@ -1,9 +1,12 @@
 /// <reference path="../../typings/jasmine.d.ts"/>
-import { isAtom } from "./match";
+import { isAtom, match2 } from "./match";
 import Cons = require("jscons");
 describe("isAtom", () => {
     it("says number is an atom", () => {
         expect(isAtom(12)).toBe(true);
+    });
+    it("says empty cons is an atom", () => {
+        expect(isAtom(Cons.from([]))).toBe(true);
     });
     it("says empty array is an atom", () => {
         expect(isAtom([])).toBe(true);
@@ -11,8 +14,8 @@ describe("isAtom", () => {
     it("says empty object is an atom", () => {
         expect(isAtom({})).toBe(true);
     });
-    it("says null is not an atom", () => {
-        expect(isAtom(null)).toBe(false);
+    it("says null is an atom", () => {
+        expect(isAtom(null)).toBe(true);
     });
     it("says cons on to Cons.from is not an atom", () => {
         expect(isAtom(new Cons(1, Cons.from([2, 3])))).toBe(false);
@@ -20,4 +23,27 @@ describe("isAtom", () => {
     it("says true is an atom", () => {
         expect(isAtom(true)).toBe(true);
     });
+});
+function basicMatchTest(match: (p: any, s: any) => boolean) {
+    it("doesn't match a list against a scalar pattern", () => {
+        expect(match(12, Cons.from([12]))).toBe(false);
+    });
+    it("doesn't match a scalar against a real pattern", () => {
+        expect(match(Cons.from([12]), 12)).toBe(false);
+    });
+    it("empty list matches an empty pattern", () => {
+        expect(match(Cons.from([]), Cons.from([]))).toBe(true);
+    }); 
+    it("non-empty list doesn't match an empty pattern", () => {
+        expect(match(Cons.from([]), Cons.from([1]))).toBe(false);
+    }); 
+    it("empty list doesn't match a non-empty pattern", () => {
+        expect(match(Cons.from([1]), Cons.from([]))).toBe(false);
+    }); 
+}
+describe("match2", () => {
+    it("matches bare atoms", () => {
+        expect(match2(12, 12)).toBe(true);
+    });
+    basicMatchTest(match2);
 });
