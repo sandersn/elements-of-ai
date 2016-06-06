42462a984f4e59fbe1649f2e5ccec132a435f9b5
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-06-05 21:40:21 -0700

    Refactor the most egregious ex-lisp code

--- src/chapter3/leibniz.ts
+++ src/chapter3/leibniz.ts
@@ -3,20 +3,15 @@ import { Map, equal } from "../util";
 /// types
 interface Rule {
     pattern: any;
-    action: (d: State) => any;
+    fire: (d: State) => any;
     name: string;
 }
 type State = Map<any>;
 type Expression = any[];
 /// rules ///
 export function existsTree(tree: any[], x: any): boolean {
-    if (isAtom(tree)) {
-        return tree === x;
-    }
-    else {
-        return tree.some(child =>
-                         isAtom(child) ? child === x : existsTree(child, x));
-    }
+    if (isAtom(tree)) return tree === x;
+    return tree.some(child => isAtom(child) ? child === x : existsTree(child, x));
 }
 export function rewriteTreeSingle(tree: Expression, f: (x: any) => any): Expression {
     function update(tree: Expression, i: number, x: any) {
@@ -70,29 +65,29 @@ const helpers: Map<(v: any, d:State) => boolean> = {
 }
 const diffSumRule: Rule = {
     pattern: ["d", ["isNestedDiffSum", "e3"], ["?", "v1"]],
-    action: d => ["+", ['d', d['e1'], d['v1']], ['d', d["e2"], d["v1"]]],
+    fire: d => ["+", ['d', d['e1'], d['v1']], ['d', d["e2"], d["v1"]]],
     name: "diffSumRule"
 };
 const diffXRule: Rule = {
     pattern: ["d", ["isNestedDiffX1", "e1"], ["isNestedDiffX2", "e2"]],
-    action: d => 1,
+    fire: d => 1,
     name: "diffXRule"
 }
 const diffConstRule: Rule = {
     pattern: ["d", ["?", 'e1'], ["diffConstE2Rule", "e2"]],
-    action: d => 0,
+    fire: d => 0,
     name: "diffConstRule"
 }
 const diffProductRule: Rule = {
     pattern: ["d", ["diffProductE3Rule", "e3"], ["?", 'v1']],
-    action: d => ["+",
+    fire: d => ["+",
                   ["*", d['e2'], ['d', d['e1'], d['v1']]],
                   ["*", d['e1'], ['d', d['e2'], d['v1']]]],
     name: "diffProductRule"
 }
 const diffPowerRule: Rule = {
     pattern: ['d', ["diffPowerE3Rule", 'e3'], ["?", 'v1']],
-    action: d => ["*",
+    fire: d => ["*",
                   d['e2'],
                   ["*",
                    ["exp", d['e1'], ['dec', d['e2']]],
@@ -101,57 +96,57 @@ const diffPowerRule: Rule = {
 }
 const laddZeroRule: Rule = {
     pattern: ["+", ["?", 'x'], 0],
-    action: d => d['x'],
+    fire: d => d['x'],
     name: "laddZeroRule"
 }
 const raddZeroRule: Rule = {
     pattern: ["+", 0, ["?", 'x']],
-    action: d => d['x'],
+    fire: d => d['x'],
     name: "raddZeroRule"
 }
 const addConstRule: Rule = {
     pattern: ["+", ["isNumber", 'x'], ["isNumber", "y"]],
-    action: d => d['x'] + d['y'],
+    fire: d => d['x'] + d['y'],
     name: "addConstRule"
 }
 const lprodZeroRule: Rule = {
     pattern: ["*", ["?", 'x'], 0],
-    action: d => 0,
+    fire: d => 0,
     name: "lprodZeroRule"
 }
 const rprodZeroRule: Rule = {
     pattern: ["*", 0, ["?", 'x']],
-    action: d => 0,
+    fire: d => 0,
     name: "rprodZeroRule"
 }
 const lprodIdRule: Rule = {
     pattern: ["*", ["?", 'x'], 1],
-    action: d => d['x'],
+    fire: d => d['x'],
     name: "lprodIdRule"
 }
 const rprodIdRule: Rule = {
     pattern: ["*", 1, ["?", 'x']],
-    action: d => d['x'],
+    fire: d => d['x'],
     name: "rprodIdRule"
 }
 const prodConstRule: Rule = {
     pattern: ["*", ["isNumber", 'x'], ["isNumber", "y"]],
-    action: d => d['x'] * d['y'],
+    fire: d => d['x'] * d['y'],
     name: "prodConstRule"
 }
 const expZeroRule: Rule = {
     pattern: ["exp", ["?", 'x'], 0],
-    action: d => 1,
+    fire: d => 1,
     name: "expZeroRule"
 }
 const expIdRule: Rule = {
     pattern: ["exp", ["?", 'x'], 1],
-    action: d => d['x'],
+    fire: d => d['x'],
     name: "expIdRule"
 }
 const decRule: Rule = {
     pattern: ["dec", ["isNumber", 'x']],
-    action: d => d['x'] - 1,
+    fire: d => d['x'] - 1,
     name: "decRule"
 }
                   
@@ -160,47 +155,31 @@ export const rules: Rule[][] = [
     [laddZeroRule, raddZeroRule, addConstRule, lprodZeroRule, rprodZeroRule, lprodIdRule, rprodIdRule, prodConstRule, expZeroRule, expIdRule, decRule]]
 
 /// control scheme ///
-function fire(rule: Rule, d: State) {
-    return rule.action(d);
-}
 export function tryRule(expression: Expression, rule: Rule): Expression {
     if (isAtom(expression)) return undefined;
     return rewriteTreeSingle(expression, e => {
         const d: Map<any> = match.call(helpers, rule.pattern, e)
         if (d) {
-            return fire(rule, d);
+            return rule.fire(d);
         }
     });
 }
 export function tryRules(formula: Expression, rules: Rule[]): Expression {
-    let rulesLeft = rules;
-    while (true) {
-        if (rulesLeft.length === 0) {
-            return undefined;
-        }
-        else {
-            const newFormula = tryRule(formula, rulesLeft[0]);
-            if (newFormula !== undefined) {
-                return newFormula;
-            }
-            else {
-                rulesLeft = rulesLeft.slice(1);
-            }
+    for (const rule of rules) {
+        const newFormula = tryRule(formula, rule);
+        if (newFormula !== undefined) {
+            return newFormula;
         }
     }
 }
-export function control(formula: Expression, rules: Rule[][]) {
-    while (rules.length) {
-        const newFormula = tryRules(formula, rules[0]);
-        if (newFormula !== undefined) {
+export function control(formula: Expression, ruleSets: Rule[][]) {
+    let newFormula: Expression = formula;
+    for (const rules of ruleSets) {
+        while (true) {
+            newFormula = tryRules(formula, rules);
+            if (newFormula === undefined) break;
             formula = newFormula;
-            continue;
-        }
-        else {
-            rules = rules.slice(1);
         }
     }
     return formula;
-            
-            
 }
