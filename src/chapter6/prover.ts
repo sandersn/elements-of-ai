c49d3ad39525d4f7233e7dd9aa73c25670d85195
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-02-26 08:49:57 -0800

    Add parser for propositions and a lot of tests
    But the prover still isn't matching correctly.

--- src/chapter6/prover.ts
+++ src/chapter6/prover.ts
@@ -15,38 +15,49 @@ export function prover() {
         rl.question("-->", answer => {
             if (answer.toLowerCase() === 'help') {
                 console.log("Here's an example:");
-                console.log("[['a', 'and', ['not', 'b']], 'implies', 'a']");
+                console.log("(a & (not b)) -> a");
             }
             if (answer.toLowerCase() === 'return') {
                 return;
             }
-            try {
-                const result = reformat(JSON.parse(answer));
-                if (valid(null, [result])) {
-                    console.log("VALID");
-                }
-                else {
-                    console.log("INVALID");
-                }
-            }
-            catch(e) {
-                // if json exception, say so
-                // if syntax error from reformat, then use its attached value (which will be null)
-                // I think we are supposed to skip; I can't remember what catch does in common lisp
-                console.log("SYNTAX ERROR");
-            }
+            console.log(prove(parse(answer)));
             rec();
         });
     })();
 }
 
-function valid(l: Proposition, r: Proposition): boolean {
+export function prove(answer: Proposition): string {
+    let result: Proposition;
+    try {
+        result = normalise(answer);
+    }
+    catch(e) {
+        if (e.message === "syntax error") {
+            result = [];
+        }
+    }
+    if (valid([], [result])) {
+        return "VALID";
+    }
+    else {
+        return "INVALID";
+    }
+    //catch(e) {
+        // if json exception, say so
+        // if syntax error from reformat, then use its attached value (which will be null)
+        // I think we are supposed to skip; I can't remember what catch does in common lisp
+        //return "SYNTAX ERROR";
+    //}
+}
+
+export function valid(l: Proposition, r: Proposition): boolean {
     // if any element is in both l and r, this proposition is valid
     for (const x of l) {
         if (r.some((y: any) => equal(x,y))) {
             return true;
         }
     }
+    console.log(l);
     let m = match.call(helpers, [["*", 'x'], ['notWff', 'y'], ["*", 'z']], l);
     if (m) {
         return valid(m.x.concat(m.z), r.concat(m.y.slice(1)));
@@ -76,7 +87,7 @@ function valid(l: Proposition, r: Proposition): boolean {
     return false;
 }
 
-function reformat(x: Proposition): Proposition {
+export function normalise(x: Proposition): Proposition {
     if (isAtom(x)) {
         return x;
     }
@@ -85,23 +96,100 @@ function reformat(x: Proposition): Proposition {
     }
     const [l, c, r] = x;
     if (notWff(x)) {
-        return ['not', reformat(c)];
+        return ['not', normalise(c)];
     }
     else if (c === 'implies') {
-        return [['not', reformat(l)], 'or', reformat(r)];
+        return [['not', normalise(l)], 'or', normalise(r)];
     }
     else {
-        return [reformat(l), c, reformat(r)];
+        return [normalise(l), c, normalise(r)];
     }
 }
-function wff(x: Proposition): boolean {
+export function wff(x: Proposition): boolean {
     return isAtom(x) ||
         (x.length === 2 && x[0] === 'not' && wff(x[1])) ||
-        (x.length === 3 && wff(x[0]) && op(x[1]) && wff(x[2]));
+        (x.length === 3 && wff(x[0]) && isOp(x[1]) && wff(x[2]));
 }
 const notWff = (x: Proposition) => !isAtom(x) && x[0] === 'not';
 const orWff = (x: Proposition) => !isAtom(x) && x[1] === 'or';
 const andWff = (x: Proposition) => !isAtom(x) && x[1] === 'and';
-const op = (x: string) => ['and', 'or', 'implies'].indexOf(x) > -1;
+const isOp = (x: string) => ['and', 'or', 'implies'].indexOf(x) > -1;
 
-let helpers = { wff, notWff, orWff, andWff, op };
+let helpers = { wff, notWff, orWff, andWff, isOp };
+
+export function parse(s: string): Proposition {
+    const tokens = tokenise(s);
+    let i = 0;
+    const e = parseExpression();
+    if (i !== tokens.length) {
+        throw new Error(`parse error: ${i} is less than ${tokens.length}`);
+    }
+    return Array.isArray(e) ? e : [e];
+
+    function next(): string {
+        const s = tokens[i];
+        i++;
+        return s;
+    }
+
+    function parseExpression(): Proposition {
+        let e: Proposition;
+        const token = next();
+        if (token === 'not') {
+            e = ['not', parseExpression()];
+        }
+        else if (token === '(') {
+            e = parseExpression();
+            if (next() !== ')') {
+                throw new Error("expected matching )");
+            }
+        }
+        else {
+            e = token;
+        }
+
+        if (isOp(mapSynonym(tokens[i]))) {
+            const l = e;
+            const op = mapSynonym(next());
+            if (!isOp(op)) {
+                throw new Error(`unexpected operator ${op}`);
+            }
+            const r = parseExpression();
+            return [l, op, r];
+        }
+        else {
+            return e;
+        }
+    }
+
+    function mapSynonym(op: string): string {
+        switch(op) {
+            case "&":
+                return "and";
+            case "|":
+                return 'or';
+            case "->":
+            case "=>":
+                return "implies";
+            default:
+                return op;
+        }
+    }
+}
+function tokenise(s: string): string[] {
+    return s.split(/ |(\)|\()/).filter(id => id);
+}
+export function format(e: Proposition): string {
+    if (isAtom(e)) {
+        return e.toString();
+    }
+    if (e.length === 2 && e[0] === "not") {
+        return "(not " + format(e[1]) + ")";
+    }
+    if (e.length === 3 && isOp(e[1])) {
+        const l = format(e[0]);
+        const r = format(e[2]);
+        return `(${l} ${e[1]} ${r})`;
+    }
+    throw new Error(`tried to format malformed expression ${e}`);
+}
---
+++ src/chapter6/test.ts
@@ -0,0 +1,102 @@
+/// <reference path="../../typings/jasmine.d.ts"/>
+import { equal, Map } from "../util";
+import { parse, format, valid, prove, normalise, wff } from "./prover"
+describe("prove", () => {
+    it("validates intersection", () => {
+        //expect(prove([[]])).toEqual("VALID");
+    });
+    it("passes the example", () => {
+        //expect(prove(parse("(a & (not b)) -> a"))).toEqual("VALID");
+    });
+});
+function runValidate(ls: string[], rs: string[], equals: boolean) {
+    expect(valid(ls.map(parse), rs.map(parse))).toEqual(equals);
+}
+function run(x: Map<[string[], string[], boolean]>) {
+    for (const k in x) {
+        it(k, () => {
+            const [ ls, rs, result ] = x[k];
+            runValidate(ls, rs, result);
+        });
+    }
+}
+describe("valid", () => {
+    run({
+        "validates intersection": [['x'], ['x', 'x & y'], true],
+        "negative validates intersection": [['x'], ['x & y'], false],
+        "validates negation": [['x', 'not x'], ['x & y'], true],
+    });
+});
+describe("normalise", () => {
+    it("is identity for atoms", () => {
+        expect(normalise('x')).toEqual('x');
+    });
+    it("throws an error for ill-formed expressions", () => {
+        expect(() => normalise(['singleton'])).toThrowError("syntax error");
+    });
+    it("rewrites implication", () => {
+        expect(normalise(parse("x -> y"))).toEqual(parse("(not x) | y"));
+    });
+    it("rewrites nested implications", () => {
+        expect(normalise(parse("(x -> y) -> z -> alpha"))).toEqual(
+            parse("(not ((not x) or y)) or ((not z) or alpha)"));
+    });
+});
+describe("wff", () => {
+    it("is true for atoms", () => {
+        expect(wff('x')).toEqual(true);
+    });
+    it("is false for 1-element lists", () => {
+        expect(wff(['x'])).toEqual(false);
+    });
+    it("is true for 2-element not-lists", () => {
+        expect(wff(parse("not x"))).toEqual(true);
+    });
+    it("is false for other 2-element lists", () => {
+        expect(wff(['x', 'y'])).toEqual(false);
+    });
+    it("is true for 3-element operator expressions", () => {
+        expect(wff(parse("x => y"))).toEqual(true);
+    });
+    it("is false for other 3-element expressions", () => {
+        expect(wff(['x', 'whatever', 'y'])).toEqual(false);
+    });
+    it("is true for well-formed nested expressions", () => {
+        expect(wff(parse("(not x) & (y -> x)"))).toEqual(true);
+    });
+});
+describe("parse", () => {
+    it("parses x => y", () => {
+        expect(parse("x -> y")).toEqual(['x', 'implies', 'y']);
+    });
+    it("parses (x) => y", () => {
+        expect(parse("(x) => y")).toEqual(['x', 'implies', 'y']);
+    });
+    it("parses not x & y", () => {
+        expect(parse("not x & y")).toEqual(['not', ['x', 'and', 'y']]);
+    });
+    it("parses (not x) & y", () => {
+        expect(parse("(not x) & y")).toEqual([['not', 'x'], 'and', 'y']);
+    });
+    it("parses (not x) and (y -> x)", () => {
+        expect(parse("(not x) and y -> x")).toEqual([["not", 'x'], 'and', ['y', 'implies', 'x']]);
+    });
+    it("parses (not x) and (y -> x)", () => {
+        expect(parse("(not x) and y -> x")).toEqual([["not", 'x'], 'and', ['y', 'implies', 'x']]);
+    });
+    it("parses x -> y -> z -> alpha", () => {
+        expect(parse("x -> y -> z -> alpha")).toEqual(['x', 'implies', ['y', 'implies', ['z', 'implies', 'alpha']]]);
+    });
+    it("parses (x -> y) -> z -> alpha", () => {
+        expect(parse("(x -> y) -> z -> alpha")).toEqual([['x', 'implies', 'y'], 'implies', ['z', 'implies', 'alpha']]);
+    });
+
+});
+describe("format", () => {
+    it("formats x=> y", () => {
+        expect(format(parse("x -> y"))).toEqual("(x implies y)");
+    });
+    it("formats (not x) => y", () => {
+        expect(format(parse("(not x) -> y"))).toEqual("((not x) implies y)");
+    });
+});
